d0b5576486ed4273eb9cfb84e27950184204bc2a
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-12-02 14:32:10 +0800

    wap

--- src/test/java/simulations/rampUsersPerSecSimulation.java
+++ src/test/java/simulations/rampUsersPerSecSimulation.java
@@ -1,6 +1,12 @@
 package simulations;
+
+import io.gatling.javaapi.core.Simulation;
+import ApiEndpoints.APiEndpoints;
+
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import static io.gatling.javaapi.core.CoreDsl.*;
@@ -8,5 +14,20 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 
 // 第一秒n个用户，第60秒内递增到m个用户，60秒内总共新增m-n用户
-public class rampUsersPerSecSimulation {
+public class rampUsersPerSecSimulation  extends Simulation{
+
+    private static final Logger log = LoggerFactory.getLogger(rampUsersPerSecSimulation.class);
+    HttpProtocolBuilder requestBuilder = http.baseUrl("http://localhost:9527/");
+    ScenarioBuilder scenarioBuilder = scenario("rampUsersPerSecExample")
+            .exec(APiEndpoints.load.check(bodyString().saveAs("responseBody")));
+//            .exec(log("Response: ${responseBody}"));
+
+    {
+        setUp(
+                scenarioBuilder.injectOpen(
+                        rampUsersPerSec(10).to(50).during(10)
+                )
+        ).protocols(requestBuilder);
+    }
+
 }
