cb31df4fabc549311bb532b5545dfd580eff6c2f
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-12-02 15:29:52 +0800

    wap

---
+++ src/test/java/simulations/ConstantCurrentUsersSimulation.java
@@ -0,0 +1,17 @@
+package simulations;
+
+
+
+import ApiEndpoints.APiEndpoints;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+// 此方法用于定义在整个测试运行期间，保持恒定的并发用户数，Gatling 会保持一个固定的数量的用户活跃，直到测试结束
+public class ConstantCurrentUsersSimulation extends Simulation {
+
+}
