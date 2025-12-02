ac0fd63c15698bd470103c4e20151f8aa16d2713
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-12-02 16:12:45 +0800

    wap

--- src/test/java/simulations/ConstantCurrentUsersSimulation.java
+++ src/test/java/simulations/ConstantCurrentUsersSimulation.java
@@ -13,5 +13,14 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 // 此方法用于定义在整个测试运行期间，保持恒定的并发用户数，Gatling 会保持一个固定的数量的用户活跃，直到测试结束
 public class ConstantCurrentUsersSimulation extends Simulation {
+    HttpProtocolBuilder httpProtocolBuilder = http.baseUrl("http://localhost:9527/");
+    ScenarioBuilder scenarioBuilder = scenario("constantCurrentUsersSimulationExample")
+            .exec(APiEndpoints.load.check(bodyString().saveAs("response")));
 
+
+    {
+        setUp(
+            scenarioBuilder.injectOpen(constantUsersPerSec(20).during(20))
+        ).protocols(httpProtocolBuilder);
+    }
 }
