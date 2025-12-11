19e88c777b792a22ec9ce24e197665a9ee25b62c
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-12-11 10:53:28 +0800

    wap

--- src/test/java/common/RandomProductIterator.java
+++ src/test/java/common/RandomProductIterator.java
@@ -1,10 +1,19 @@
 package common;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.logging.SimpleFormatter;
 
 public class RandomProductIterator implements Iterator<Map> {
+
+    private static final SimpleDateFormat simpleFormatter = new SimpleDateFormat("YYYYMMDD");
+    private static final Random random = new Random();
+
+
 
     @Override
     public boolean hasNext() {
@@ -13,6 +22,8 @@ public class RandomProductIterator implements Iterator<Map> {
 
     @Override
     public Map next() {
+        String date = simpleFormatter.format(new Date());
+        String No = simpleFormatter.format(new Date())+String.format("%06d",random.nextInt());
         return Map.of();
     }
 
